3200b97d2eac3d0d98ee8f9fcd81c3d495085de2
mucollabo <mucollabo@gmail.com>
2022-08-18 17:53:28 +0900

    complete web308

--- source/02_nodejs-mysql/main.js
+++ source/02_nodejs-mysql/main.js
@@ -54,7 +54,7 @@ const app = http.createServer(function(request, response) {
                             <form action="delete_process" method="post">
                                 <input type="hidden" name="id" value="${queryData.id}>
                                 <textarea name="description"></textarea>
-                                <input type="submit">
+                                <input type="submit" value="delete">
                             </form>`
                         );
                     response.writeHead(200);
@@ -63,9 +63,9 @@ const app = http.createServer(function(request, response) {
             });
         }
     } else if(pathname === '/create') {
-        fs.readdir('./data', function(error, filelist) {
-            const title = 'WEB - create';
-            const list = template.list(filelist);
+        db.query(`SELECT * FROM topic`, function(error, topics) {
+            const title = 'Create';
+            const list = template.list(topics);
             const html = template.HTML(title, list, `
                 <form action="/create_process" method="post">
                     <p><input type="text" name="title" placeholder="title"></p>
@@ -76,76 +76,83 @@ const app = http.createServer(function(request, response) {
                         <input type="submit">
                     </p>
                 </form>
-            `, '');
+                `,
+                `<a href="/create">create</a>`
+            );
             response.writeHead(200);
             response.end(html);
         });
     } else if(pathname === '/create_process') {
-        const body = '';
+        let body = '';
         request.on('data', function(data) {
             body = body + data;
         });
         request.on('end', function() {
             const post = qs.parse(body);
-            const title = post.title;
-            const description = post.description;
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-                response.writeHead(302, {Location: `/?id=${title}`});
-                response.end();
-            });
+            db.query(`INSERT INTO topic (title, description, created, author_id) VALUES(?, ?, NOW(), ?)`,
+                [post.title, post.description, 1],
+                function(error, result) {
+                    if(error) {
+                        throw error;
+                    }
+                    response.writeHead(302, {Location: `/?id=${result.insertId}`});
+                    response.end();
+                }
+            );
         });
     } else if(pathname === '/update') {
-        fs.readdir('./data', function(error, filelist) {
-            const filteredId = path.parse(queryData.id).base;
-            fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
-                const title = queryData.id;
-                const list = template.list(filelist);
-                const html = template.HTML(title, list,
+        db.query('SELECT * FROM topic', function(error, topics) {
+            if(error) {
+                throw error;
+            }
+            db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id], function(error2, topic) {
+                if(error2) {
+                    throw error2;
+                }
+                const list = template.list(topics);
+                const html = template.HTML(topic[0].title, list,
                     `
                     <form action="/update_process" method="post">
-                        <input type="hidden" name="id" value="${title}">
-                        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                        <input type="hidden" name="id" value="${topic[0].id}">
+                        <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
                         <p>
-                            <textarea name="description" placeholder="description">${description}</textarea>
+                            <textarea name="description" placeholder="description">${topic[0].description}</textarea>
                         </p>
                         <p>
                             <input type="submit">
                         </p>
                     </form>
                     `,
-                    `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                    `<a href="/create">create</a> <a href="/update?id=${topic[0].title}">update</a>`
                 );
                 response.writeHead(200);
                 response.end(html);
             });
         });
     } else if(pathname === '/update_process') {
-        const body = '';
+        let body = '';
         request.on('data', function(data) {
             body = body + data;
         });
         request.on('end', function() {
             const post = qs.parse(body);
-            const id = post.id;
-            const title = post.title;
-            const description = post.description;
-            fs.rename(`data/${id}`, `data/${title}`, function(error) {
-                fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-                    response.writeHead(302, {Location: `/?id=${title}`});
-                    response.end();
-                });
+            db.query('UPDATE topic SET title=?, description=?, author_id=1 WHERE id=?',
+                [post.title, post.description, post.id], function(error, result) {
+                response.writeHead(302, {Location: `/?id=${post.id}`});
+                response.end();
             });
         });
     } else if(pathname === '/delete_process') {
-        const body = '';
+        let body = '';
         request.on('data', function(data) {
             body = body + data;
         });
         request.on('end', function() {
             const post = qs.parse(body);
-            const id = post.id;
-            const filteredId = path.parse(id).base;
-            fs.unlink(`data/${filteredId}`, function(error) {
+            db.query('DELETE FROM topic WHERE id=?', [parseInt(post.id)], function(error, result) {
+                if(error) {
+                    throw error;
+                }
                 response.writeHead(302, {Location: `/`});
                 response.end();
             });
